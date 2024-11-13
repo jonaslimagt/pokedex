cfa28f7e8ce8783bc4675d23a467eb4d3d5ce53c
Jonas Lima <jonaslimagt@gmail.com>
2024-11-13 17:27:23 -0300

    Implementa numeração, tipo e imagens de forma dinâmica

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,18 @@
-
+function convertPokemonTypestoHtml(pokemonTypes){
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`);
+}
 
 function convertPokemonToHtml(pokemon){
     return `
         <li class="pokemon">
-            <span class="number">#001</span>
+            <span class="number">#${pokemon.id}</span>
             <span class="name">${pokemon.name}</span>
             <div class="detail">
                 <ol class="types">
-                    <li class="type">Glass</li>
-                    <li class="type">Poison</li>
+                    ${convertPokemonTypestoHtml(pokemon.types).join('')}
                 </ol>
 
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="${pokemon.name}">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png" alt="${pokemon.name}">
             </div>
         </li>
     `
@@ -20,5 +21,5 @@ function convertPokemonToHtml(pokemon){
 const pokemonList = document.getElementById('pokemonList');
 
 pokeApi.getPokemons().then((pokemons = []) => {
-    pokemonList.innerHTML += pokemons.map(convertPokemonToHtml).join("");
+    pokemonList.innerHTML += pokemons.map(convertPokemonToHtml).join('');
 })
