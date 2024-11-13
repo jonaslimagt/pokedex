8c86af192270581b182111222f4e8e2dd7d032ae
Jonas Lima <jonaslimagt@gmail.com>
2024-11-13 15:03:41 -0300

    Inicia a integração entre o HTML, o JavaScript e o PokeAPI

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,14 +2,32 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
+function convertPokemonToHtml(pokemon){
+    return `
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types">
+                    <li class="type">Glass</li>
+                    <li class="type">Poison</li>
+                </ol>
+
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList');
+
 fetch(url)
     .then((response) => response.json())
-    .then((jsonBody) => console.log(jsonBody))
-
-    .catch(function(error){
-        console.error(error);
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => {
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+            pokemonList.innerHTML += convertPokemonToHtml(pokemon);
+        }
     })
-
-    .finally(function() {
-        console.log('Request finished!');
-    })
+    .catch((error) => console.error(error))
