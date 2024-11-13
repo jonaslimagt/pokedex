631b5bc83a0863181d6bf581858f67368a652542
Jonas Lima <jonaslimagt@gmail.com>
2024-11-13 15:22:16 -0300

    Encapisula as funções que manuseam a PokeAPI

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,4 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
 
 function convertPokemonToHtml(pokemon){
     return `
@@ -21,10 +19,7 @@ function convertPokemonToHtml(pokemon){
 
 const pokemonList = document.getElementById('pokemonList');
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
+pokeApi.getPokemons().then((pokemons) => {
         for (let i = 0; i < pokemons.length; i++) {
             const pokemon = pokemons[i];
             pokemonList.innerHTML += convertPokemonToHtml(pokemon);
