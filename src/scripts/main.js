dd681ae92f179c781ada6ce708bce7d77752aae4
Jonas Lima <jonaslimagt@gmail.com>
2024-11-19 15:13:55 -0300

    Implementa o botão 'Load more'

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,22 +1,29 @@
+const pokemonList = document.getElementById('pokemonList');
+const loadMoreButton = document.getElementById('loadMore');
+let limit = 12;
+let offset = 0;
 
-function convertPokemonToHtml(pokemon) {
-    return `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.id.toString().padStart(4, '0')}</span>
-            <span class="name">${pokemon.name}</span>
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
+function loadMorePokemons(offset = 0, limit = 251){
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        pokemonList.innerHTML += pokemons.map((pokemon) => `
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.id.toString().padStart(4, '0')}</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
 
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png" alt="${pokemon.name}">
-            </div>
-        </li>
-    `
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png" alt="${pokemon.name}">
+                </div>
+            </li>
+        `).join('');
+    })
 }
 
-const pokemonList = document.getElementById('pokemonList');
+loadMorePokemons(offset, limit);
 
-pokeApi.getPokemons().then((pokemons = []) => {
-    pokemonList.innerHTML += pokemons.map(convertPokemonToHtml).join('');
-})
+loadMoreButton.addEventListener('click', () => {
+    offset += limit;
+    loadMorePokemons(offset, limit);
+});
--- src/scripts/poke-api.js
+++ src/scripts/poke-api.js
@@ -19,7 +19,7 @@ pokeApi.getPokemonDetail = (pokemon) => {
         .then(convertApiToPokemon);
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 251) => {
+pokeApi.getPokemons = (offset = 0, limit = 10) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
     return fetch(url)
         .then((response) => response.json())
